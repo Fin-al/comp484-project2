f0ebee6e813b2dfb9fd52b2c10e304fe8ded133d
Fin-al <finaltbm@gmail.com>
2026-04-16 00:45:20 -0700

    work
    work

--- script.js
+++ script.js
@@ -7,30 +7,51 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     $('.treat-button').click(clickedTreatButton);
     $('.play-button').click(clickedPlayButton);
     $('.exercise-button').click(clickedExerciseButton);
+    $('.nap-button').click(clickedNapButton);
+    $('.tron-button').click(clickedTronButton);
   
-
-  
-    
-  })
+})
   
     // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-    var pet_info = {name:"My Pet Name", weight:"??", happiness:"??"};
+    var pet_info = {name:"Bit", weight: 15, happiness: 20};
   
     function clickedTreatButton() {
       // Increase pet happiness
+      pet_info.happiness += 2;
       // Increase pet weight
+      pet_info.weight += 3;
+      updatePetMessage("Tasty snack!");
       checkAndUpdatePetInfoInHtml();
     }
     
     function clickedPlayButton() {
       // Increase pet happiness
+      pet_info.happiness += 5;
       // Decrease pet weight
+      pet_info.weight -= 2;
+      updatePetMessage("That was fun!");
       checkAndUpdatePetInfoInHtml();
     }
     
     function clickedExerciseButton() {
       // Decrease pet happiness
+      pet_info.happiness -= 3;
       // Decrease pet weight
+      pet_info.weight -= 5;
+      updatePetMessage("Good workout!");
+      checkAndUpdatePetInfoInHtml();
+    }
+
+    function clickedNapButton() {
+      pet_info.happiness += 1;
+      updatePetMessage("Feeling rested.");
+      checkAndUpdatePetInfoInHtml();
+    }
+
+    function clickedTronButton() {
+      pet_info.happiness += 10;
+      pet_info.weight -= 8;
+      updatePetMessage("Greetings, Program! Light Cycle racing is intense!");
       checkAndUpdatePetInfoInHtml();
     }
   
@@ -41,6 +62,23 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     
     function checkWeightAndHappinessBeforeUpdating() {
       // Add conditional so if weight is lower than zero.
+      if (pet_info.weight < 0) {
+        pet_info.weight = 0;
+      }
+      if (pet_info.happiness < 0) {
+        pet_info.happiness = 0;
+      }
+
+      // Explosion Logic
+      if (pet_info.weight > 30 || pet_info.happiness <= 0 || pet_info.happiness >= 100||pet_info.weight <=0 ) {
+        $('.pet-image').wrap("<div style='background: #00ffff; border: 50px solid white; border-radius: 50%; padding: 20px;'></div>");
+        
+        setTimeout(function() {
+          $('.pet-image').parent().remove();
+          $('.button-container').empty();
+          $('#pet-comment').text("DEREZZED! " + pet_info.name + " has left the grid.");
+        }, 300);
+      }
     }
     
     // Updates your HTML with the current values in your pet_info object
@@ -49,4 +87,19 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
       $('.weight').text(pet_info['weight']);
       $('.happiness').text(pet_info['happiness']);
     }
-  
+
+    function updatePetMessage(msg) {
+       
+      $('#pet-comment').empty();
+      $('#pet-comment').text(msg);
+
+      if ($('.pet-image').length > 0) {
+         
+        $('.pet-image').wrap("<div style='border: 3px dashed #00ffff; display: inline-block;'></div>");
+        
+        setTimeout(function() {
+           
+          $('.pet-image').unwrap();
+        }, 500);
+      }
+    }
